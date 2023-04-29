45bad95cb2b6e438ceec28d0f7538a0a91f3ebc8
FlorentTirvaudey <florent.tirvaudey@gmail.com>
2023-04-29 11:51:28 +0200

    feat(validate): add DOM Elements

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const mailUser = document.getElementById("email");
+const birthdateUser = document.getElementById("birthdate");
+const turnamentNumber = document.getElementById("number");
+const loc1 = document.getElementById("location1");
+const loc2 = document.getElementById("location2");
+const loc3 = document.getElementById("location3");
+const loc4 = document.getElementById("location4");
+const loc5 = document.getElementById("location5");
+const loc6 = document.getElementById("location6");
+const check1 = document.getElementById("checkbox1");
+const check2 = document.getElementById("checkbox2");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
